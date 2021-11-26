9fdd0b439b2175563e789859dbba0594a7a5807b
EZRA-WERE <85166735+EZRA-WERE@users.noreply.github.com>
2021-11-26 20:46:58 +0300

    Initial commit

---
+++ AGE BRACKETS.py
@@ -0,0 +1,14 @@
+age= int(input("how old are you?:"))
+if age<=0:
+    print("you haven't been born!")
+elif age<=12:
+    print("you're a child!")
+elif age<=19:
+  print("you're a teeneger!")
+elif age<=35:
+    print("you're a youth!")
+elif age<=120:
+  print("you're old!")
+else:
+  print("you're extremely old!")
+
